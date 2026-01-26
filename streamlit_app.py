ad4e95ce18b31dcc3f628088e3daa66082209ff4
Andres Soto Plaza <119691164+agsotopl@users.noreply.github.com>
2026-01-26 04:37:23 +0000

    final

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import PyPDF2
 
 
 def read_pdf(uploaded_file):
-    """Extract text from a PDF file."""
+    """ PDF file Extraction"""
     pdf_reader = PyPDF2.PdfReader(uploaded_file)
     document = ""
     for page in pdf_reader.pages:
